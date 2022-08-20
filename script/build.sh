6c0c2ee529de64ba76805a48c85187d90bb8c91f
sunbo.kie <sunbo.kie@bytedance.com>
2022-08-20 03:15:28 +0000

    [ADD] extract configure and build step to build.sh

---
+++ script/build.sh
@@ -0,0 +1,30 @@
+WORKSPACE=$1
+BUILD_TYPE=$2
+
+echo "【KIE】Create Build Environment"
+cmake -E make_directory ${WORKSPACE}/build
+
+
+echo "【KIE】Install Conan"
+sudo apt update
+sudo apt install -y python3 python3-pip lcov curl
+pip install conan
+
+
+echo "【KIE】Configure Conan"
+conan profile new default --detect &&
+conan profile update settings.compiler.libcxx=libstdc++11 default &&
+conan profile update settings.build_type=${BUILD_TYPE} default &&
+conan remote add kie-kies https://conan.cloudsmith.io/kie/kies/
+
+echo "【KIE】Install Dependency"
+cd ${WORKSPACE}/build
+conan install .. --build=missing -pr:b=default -pr:h=default
+
+echo "【KIE】Configure CMake"
+cd ${WORKSPACE}/build
+cmake ${WORKSPACE} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE=generators/conan_toolchain.cmake
+
+echo "【KIE】Build"
+cd ${WORKSPACE}/build
+cmake --build . --config ${BUILD_TYPE}
