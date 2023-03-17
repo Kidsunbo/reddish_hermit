323dfda3d2a41b7725ec6380f236e31cffd1dfa4
sunbo.kie <sunbo.kie@bytedance.com>
2023-03-17 03:54:42 +0000

    [ADD] update the build script and workflows for github action

--- script/build.sh
+++ script/build.sh
@@ -1,17 +1,11 @@
-WORKSPACE=$1
-BUILD_TYPE=$2
-preset=$(echo "$BUILD_TYPE" | awk 'conan_{print tolower($0)}')
-
-
-echo "【KIE】Create Build Environment"
-cmake -E make_directory ${WORKSPACE}/build
+BUILD_TYPE=$1
+preset=$(echo "$BUILD_TYPE" | awk '{print "conan-" tolower($0)}')
 
 
 echo "【KIE】Install Conan"
 sudo apt update
 sudo apt install -y python3 python3-pip lcov curl
 pip install conan
-
 
 echo "【KIE】Configure Conan"
 conan profile detect
