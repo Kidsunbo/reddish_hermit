9cdcca9a74ebde757b9f3e02cb1d34fec344fdff
Kidsunbo <qiongxiaozi158@sina.com>
2024-06-06 03:15:14 +0000

    [FIX] fix compile script

--- script/build_client.sh
+++ script/build_client.sh
@@ -2,13 +2,13 @@ BUILD_TYPE=$1
 preset=$(echo "$BUILD_TYPE" | awk '{print "conan-" tolower($0)}')
 
 
-# echo "【KIE】Install Conan"
-# sudo apt update
-# sudo apt install -y python3 python3-pip lcov curl
-# pip install conan
+echo "【KIE】Install Conan"
+sudo apt update
+sudo apt install -y python3 python3-pip lcov curl
+pip install conan
 
-# echo "【KIE】Configure Conan"
-# conan profile detect
+echo "【KIE】Configure Conan"
+conan profile detect
 
 echo "【KIE】Install Dependency"
 conan install . --build=missing -s build_type=${BUILD_TYPE}
