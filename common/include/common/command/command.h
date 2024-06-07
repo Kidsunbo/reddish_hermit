d75b311f50a17212288b11241d5561e8295ed7e3
Kidsunbo <qiongxiaozi158@sina.com>
2024-06-07 06:51:10 +0000

    [ADD] add token

--- common/include/common/command/command.h
+++ common/include/common/command/command.h
@@ -2,22 +2,18 @@
 
 #include <string_view>
 
-#define __Reddish_RegisterCommands(x) constexpr std::string_view x = #x;
-
 namespace reddish::common::commands {
-    // G
-    __Reddish_RegisterCommands(GET)
-    __Reddish_RegisterCommands(GETBIT)
-    __Reddish_RegisterCommands(GETDEL)
-    __Reddish_RegisterCommands(GETEX)
-    __Reddish_RegisterCommands(GETRANGE)
-    __Reddish_RegisterCommands(GETSET)
+   
+   enum class Commands {
+        Get,
+        GetBit,
+        Set,
+        SetBit,
+   };
 
-    // S
-    __Reddish_RegisterCommands(SET)
-    __Reddish_RegisterCommands(SETBIT)
-    __Reddish_RegisterCommands(SETEX)
-    __Reddish_RegisterCommands(SETNX)
-    __Reddish_RegisterCommands(SETRANGE)
+
+    bool equal(Commands, std::string_view);
+
+    std::string_view to_string(Commands);
 
 }
--- common/src/command/command.cpp
+++ common/src/command/command.cpp
@@ -1,1 +1,15 @@
 #include <common/command/command.h>
+
+namespace reddish::common::commands {
+
+    bool equal(Commands, std::string_view)
+    {
+        return false;
+    }
+
+    std::string_view to_string(Commands)
+    {
+        return "";
+    }
+
+}
