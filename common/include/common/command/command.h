78c8fcc7bd2b19071517972b00d2add0b18f71fa
Kidsunbo <qiongxiaozi158@sina.com>
2024-06-08 16:13:40 +0000

    [ADD] add the way to support commands

--- common/include/common/command/command.h
+++ common/include/common/command/command.h
@@ -1,19 +1,58 @@
 #pragma once
 
+#include <concepts>
+#include <string>
 #include <string_view>
+#include <type_traits>
 
 namespace reddish::common::commands {
-   
-   enum class Commands {
+
+    template <typename T>
+    concept HasTextMethod = requires(T t) {
+         { T::text() } -> std::convertible_to<std::string_view>;
+    };
+
+    template<typename T>
+    concept IsString = std::is_same_v<std::remove_reference_t<T>, std::string_view> || std::is_same_v<std::remove_reference_t<T>, std::string>;
+
+
+    enum class CommandEnum {
         Get,
         GetBit,
         Set,
         SetBit,
-   };
+    };
+
+    template <HasTextMethod Command, std::size_t SizeHint = 30, IsString... ARGS>
+    std::string to_request_string(ARGS&&... args)
+    {
+        std::string buf;
+        buf.reserve(SizeHint);
+        const std::size_t arr_size = 1 + sizeof...(args);
+        buf.append("*");
+        buf.append(std::to_string(arr_size));
+        buf.append("\r\n");
+
+        buf.append(Command::text());
+        (buf.append(args), ...);
+        return buf;
+    }
+
+    template <CommandEnum Value>
+    class Command;
+
+    template <>
+    class Command<CommandEnum::Get> {
+    public:
+        constexpr static std::string_view text() { return "GET"; }
+        static std::string to_string(std::string_view key) { return to_request_string<Command<CommandEnum::Get>>(key); }
+    };
 
 
-    bool equal(Commands, std::string_view);
-
-    std::string_view to_string(Commands);
-
+    template <>
+    class Command<CommandEnum::GetBit> {
+    public:
+        constexpr static std::string_view text() { return "GETBIT"; }
+        static std::string to_string(std::string_view key) { return to_request_string<Command<CommandEnum::GetBit>>(key); }
+    };
 }
--- common/src/command/command.cpp
+++ common/src/command/command.cpp
@@ -1,15 +1,7 @@
 #include <common/command/command.h>
+#include <stdexcept>
 
 namespace reddish::common::commands {
 
-    bool equal(Commands, std::string_view)
-    {
-        return true;
-    }
-
-    std::string_view to_string(Commands)
-    {
-        return "";
-    }
 
 }
